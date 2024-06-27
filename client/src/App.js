d97e3034a0d5b59bc04696aabd062de13b7f3bf1
Mehdi Akacem <96568645+mehdiakacem@users.noreply.github.com>
2024-06-27 14:08:10 +0100

    added single post page and edit post page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./UserContext";
 import CreatePost from "./pages/CreatePost";
+import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost"
 
 function App() {
   return (
@@ -17,8 +19,10 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/create" element={<CreatePost />} />	
-		</Route>
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+        </Route>
       </Routes>
     </UserContextProvider>
   );
---
+++ client/src/Editor.js
@@ -0,0 +1,35 @@
+import ReactQuill from "react-quill";
+
+
+export default function Editor({value, onChange}) {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  return <ReactQuill value={value} modules={modules} onChange={onChange} />;
+}
